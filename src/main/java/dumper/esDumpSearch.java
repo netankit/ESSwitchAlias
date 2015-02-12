11f788ec7b1c330fa0eaf939af69c96ce6ce1d1b
netankit <ankit.bahuguna@cs.tum.edu>
2015-02-12 14:54:06 +0100

    Performed minor refactoring in esDumpSearch.java

--- src/main/java/dumper/esDumpSearch.java
+++ src/main/java/dumper/esDumpSearch.java
@@ -11,6 +11,7 @@ import org.elasticsearch.node.Node;
 
 public class esDumpSearch {
 	public static void main(String[] args) {
+
 		/*
 		 * Setting up ES Client
 		 */
@@ -21,23 +22,24 @@ public class esDumpSearch {
 		 * GET Response - Getting an entire row based on its id
 		 */
 		System.out.println("\nGET RESPONSE - Field ID 1");
-		GetResponse responseGet = client.prepareGet("alias2", "val2", "1")
-				.execute().actionGet();
-		System.out.println(responseGet.getSourceAsString());
+		getFieldResponse(client);
 
 		/*
 		 * Remove the old alias and add a new alias for the same index.
 		 */
 
-		client.admin().indices().prepareAliases()
-				.removeAlias("index2", "alias2")
-				.addAlias("index2", "alias_new").execute().actionGet();
+		removingThenAddingAlias(client);
 
 		/*
 		 * Searching the index - Custom word search
 		 */
 
 		System.out.println("\n\nSEARCH RESPONSE Query:>> field3:Bank");
+		searchQueryResponse(client);
+
+	}
+
+	private static void searchQueryResponse(Client client) {
 		SearchResponse responseSearch = client.prepareSearch("alias_new")
 				.setTypes("val2")
 				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
@@ -46,6 +48,17 @@ public class esDumpSearch {
 		System.out.println("Total search hits for Query: "
 				+ responseSearch.getHits().getTotalHits());
 		System.out.println("Search Response:\n" + responseSearch.toString());
+	}
 
+	private static void removingThenAddingAlias(Client client) {
+		client.admin().indices().prepareAliases()
+				.removeAlias("index2", "alias2")
+				.addAlias("index2", "alias_new").execute().actionGet();
+	}
+
+	private static void getFieldResponse(Client client) {
+		GetResponse responseGet = client.prepareGet("alias2", "val2", "1")
+				.execute().actionGet();
+		System.out.println(responseGet.getSourceAsString());
 	}
 }
